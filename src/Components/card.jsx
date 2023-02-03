3d02635f06474ef222f8d02fff92c5db88377fa6
Avik Ghosh <98302991+Avik1320@users.noreply.github.com>
2023-02-03 11:10:39 +0530

    first commit

---
+++ src/Components/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      Card page
+    </div>
+  )
+}
+
+export default card
---
+++ src/Layouts/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Style/css/main.css'
+
+const Navbar = () => {
+  return (
+    <div className='Nav'>
+      Navbar page
+    </div>
+  )
+}
+
+export default Navbar
